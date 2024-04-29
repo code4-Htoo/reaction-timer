fc3273a41253632a6cc08dd89fb10997544a290d
HTOO <htooaungwai.prof@gmail.com>
2024-04-29 10:16:40 +0630

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>Let's test your reflection!</h2>
+  <button class="btn" :disabled="isPlaying" @click="play">Play</button>
+  <PlayBlock v-if="isPlaying" :delay="delay" @end="end" />
+  <Score v-if="showScore" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PlayBlock from './components/PlayBlock.vue'
+import Score from './components/ReactionScore.vue'
 export default {
   name: 'App',
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score:null,
+      showScore:false,
+    }
+  },
   components: {
-    HelloWorld
+    PlayBlock,Score
+  },
+  methods: {
+    play() {
+      this.isPlaying = true
+      this.delay = 2000 + Math.random() * 5000
+      this.showScore=false
+    },
+    
+    end(reactionTime) {
+      this.isPlaying = false
+      this.score= reactionTime
+      this.showScore= true
+    }
   }
 }
 </script>
---
+++ src/components/ReactionScore.vue
@@ -0,0 +1,26 @@
+<template>
+  <p class="score">{{ score }}</p>
+  <h3 class="rank">{{ rank }}</h3>
+</template>
+
+<script>
+export default {
+props:['score'],
+data() {
+    return {
+        rank: null,
+    }
+},
+mounted () {
+    if(this.score < 260){
+        this.rank= "Sayar Gyi"
+    }else if(this.score <400){
+        this.rank = "OK BUT"
+    }else this.rank = "Noob!!!!!!!!!"
+}   
+}
+</script>
+
+<style>
+
+</style>
